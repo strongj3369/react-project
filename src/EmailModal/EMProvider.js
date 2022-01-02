d1ceeb6fd5b57a64bcd680c6b1240965faa066e4
Jennifer Strong <32823335+strongj3369@users.noreply.github.com>
2022-01-02 15:59:07 -0500

    completed email-provider files

--- src/EmailModal/EMProvider.js
+++ src/EmailModal/EMProvider.js
@@ -1,24 +1,68 @@
 import React, { useContext, useState } from "react";
+import Cookies from "js-cookie";
 
 export const StateContext = React.createContext();
 
 export function useStateContext() {
-  return useContext(StateContext);
+    return useContext(StateContext)
 }
 
 export function EMProvider({ children }) {
-  const openModalAction = () => {
-    setNewState({
-      ...newState,
-      openModal: true
-    });
-  };
-  const [newState, setNewState] = useState({
-    openModal: false,
-    openModalAction
-  });
+    const [modalOpen, setModalOpen] = useState(false)
+    const openModalAction = () => {
+        Cookies.set('modalOpenBefore', true, { expires: 7 })
+        setModalOpen(true)
+    }
+    const closeModalAction = () => {
+        setModalOpen(false)
+    }
 
-  return (
-    <StateContext.Provider value={newState}>{children}</StateContext.Provider>
-  );
+    const [email, setEmail] = useState('')
+    const handleEmailInput = (e) => {
+        setEmail(e.target.value)
+    }
+    const [showEmailError, setShowEmailError] = useState(false)
+    const checkForEmail = (e) => {
+        function emailIsValid(text) {
+            return /\S+@\S+\.\S+/.test(text)
+        }
+        if(!emailIsValid (email)) {
+        setShowEmailError (true)
+        }
+        console.log('checking for email')
+    }
+    const removeErrorMessage = (e) => {
+        setShowEmailError (false)
+        console.log('Removing Error Message')
+    }
+
+    const [formCompleted, setFormCompleted] = useState(false)
+    const submittedForm = (e) => {
+        e.preventDefault()
+        if(showEmailError === false && email.length > 5 ) {
+            setFormCompleted(true)
+            setTimeout(() => {
+                closeModalAction()
+            }, 3000)
+        }
+    }
+
+    return (
+        <StateContext.Provider 
+        value={{ 
+            modalOpen, 
+            email, 
+            handleEmailInput, 
+            openModalAction, 
+            closeModalAction,
+            checkForEmail,
+            showEmailError,
+            removeErrorMessage,
+            submittedForm,
+            formCompleted
+            }}>
+            {children}
+        </StateContext.Provider>
+    )
 }
+
